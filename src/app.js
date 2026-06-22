044c1e9f67e0a248ccae9f6efb9d194aa5d20d1e
HeloisaRebello17 <heloisarebellocabral.79@gmail.com>
2026-06-22 17:33:15 -0300

    add: adicionado railway

--- src/app.js
+++ src/app.js
@@ -10,7 +10,11 @@ const { requestLogger, errorLogger } = require('./middleware/logging');
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 app.use(express.json());
 app.use(requestLogger); // Add request logging middleware
 app.use(express.static(path.join(__dirname, '..', 'public')));
